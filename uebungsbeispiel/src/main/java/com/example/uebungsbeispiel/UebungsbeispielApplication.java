61a2d7eeba9a0cb4a6e4254daf55a07061154c6a
sarah <sarah.polta@gmail.com>
2022-06-17 09:49:02 +0200

    Initial commit

---
+++ uebungsbeispiel/src/main/java/com/example/uebungsbeispiel/UebungsbeispielApplication.java
@@ -0,0 +1,13 @@
+package com.example.uebungsbeispiel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UebungsbeispielApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UebungsbeispielApplication.class, args);
+	}
+
+}
---
+++ uebungsbeispiel/src/test/java/com/example/uebungsbeispiel/UebungsbeispielApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uebungsbeispiel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UebungsbeispielApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
